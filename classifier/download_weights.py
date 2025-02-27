b34e85b4f056e518c60a6a0dcd096b5620505d85
kandarpa02 <kandarpaexe@gmail.com>
2025-02-27 23:40:00 +0530

    update: fixed path

--- classifier/download_weights.py
+++ classifier/download_weights.py
@@ -2,7 +2,7 @@ import os
 import gdown
 
 WEIGHTS_URL = "https://drive.google.com/file/d/1ExRLjPnPcV5-XSvQKlv_izYRfHtlAVUB/view?usp=drive_link"
-WEIGHTS_PATH = "Fine-Tuning-VGG16-for-Fine-Grained-Flower-Classification-A-Transfer-Learning-Approach/classifier/model/model.pth"
+WEIGHTS_PATH = "classifier/model/model.pth"
 
 if not os.path.exists(WEIGHTS_PATH):
     gdown.download(WEIGHTS_URL, WEIGHTS_PATH, quiet=False, fuzzy = True)
